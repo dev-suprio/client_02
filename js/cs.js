7d831badb10dedc36be6bcb4a11c1c168c24e551
dev-suprio <103194547+dev-suprio@users.noreply.github.com>
2022-04-07 19:18:41 +0600

    Add files via upload

---
+++ js/cs.js
@@ -0,0 +1,13 @@
+var typed = new Typed('.typing', {
+    // Waits 1000ms after typing "First"
+    strings: [
+        'Firewalls', 
+        'Log Analysis'
+    
+    ],
+
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+
+  });
